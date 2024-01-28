1a63dd1bd1d753355629aebaddf7118abfcd3fea
zahra <99148531+zahrasadat80@users.noreply.github.com>
2024-01-28 12:08:20 +0330

    I finish remove from array exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr,deleteItem) {
+    let indexOfDeletedItem = arr.indexOf(deleteItem);
+    arr.splice(indexOfDeletedItem,1);
+    return arr; 
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
