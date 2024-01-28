0a562b48c9a32cda936c97806c72fdec8feba2f9
zahra <99148531+zahrasadat80@users.noreply.github.com>
2024-01-28 13:11:37 +0330

    write two functions for convert celsius to fahrenheit and opposite

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let c = (f-32)*(5/9);
+  if (c%1 !==0){//this condition check if number is not whole number 
+    return Number(c.toFixed(1));//to fixed return string 
+  }
+  return c;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let f = c * (9/5) + 32;
+  if (f%1 !==0){
+    return Number(f.toFixed(1));
+  }
+  return f;
 };
 
 // Do not edit below this line
