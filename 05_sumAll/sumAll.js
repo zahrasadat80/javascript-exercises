03b1fafd08db30bc882496928553640699713b92
zahra <99148531+zahrasadat80@users.noreply.github.com>
2024-01-28 12:32:33 +0330

    write a function for receive two number and sum all numbers between them

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
-
-};
+const sumAll = function(firstNum,lastNum) {
+    if (typeof firstNum == 'number' && typeof lastNum == 'number'){
+        if(firstNum >= 0 && lastNum >= 0){
+            let sum = 0;
+            if(firstNum>lastNum){
+                for (i=lastNum; i<=firstNum; i++){
+                    sum += i;
+                }
+            }else{
+                for (i=firstNum; i<=lastNum; i++){
+                    sum += i;
+                    }  
+            } 
+            return sum; 
+        }else{
+            return 'ERROR';
+        }
+    }else{
+        return 'ERROR';
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
